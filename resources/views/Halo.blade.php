1d96daf258834f3736ba201f73277fd509f58ae9
Muhammad Naufal NMI <mmikoyan95@gmail.com>
2025-09-01 11:05:21 +0800

    Initial commit

---
+++ resources/views/Halo.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel nih Bos</title>
+    
+</head>
+<body>
+    <h1>Halo Bos</h1>
+    <p>Ini halaman Halo Bos</p>
+    <a href="/">Kembali ke halaman utama</a>
+    <br>
+    <a href="/about">Kembali ke halaman about</a>
+
+</body>
+</html>
